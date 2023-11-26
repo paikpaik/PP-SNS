12f84040b55bef684512d6b23fd831925b97bbaa
paikpaik <woduf9311@gmail.com>
2023-11-26 21:11:41 +0900

    Init: auth module and auth service 구현해야 할 기능 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PostsModel } from './posts/entities/posts.entity';
 import { UsersModule } from './users/users.module';
 import { UsersModel } from './users/entities/users.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { UsersModel } from './users/entities/users.entity';
       synchronize: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
