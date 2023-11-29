51caefc13033edbb715174c7a48ed46c74795963
paikpaik <woduf9311@gmail.com>
2023-11-30 00:41:34 +0900

    Add: jwtService DI

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+}
 
 /**
  * 구현해야 할 기능
