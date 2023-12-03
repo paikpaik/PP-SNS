10b997df768cf002c6aa5ebda9abceaec68a93fe
paikpaik <woduf9311@gmail.com>
2023-12-03 21:53:29 +0900

    Add: passwordPipe

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -8,6 +8,9 @@ import {
 @Injectable()
 export class PasswordPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+    if (value.toString().length > 8) {
+      throw new BadRequestException('비밀번호는 8자 이하로 입력해주세요.');
+    }
+    return value.toString();
   }
 }
