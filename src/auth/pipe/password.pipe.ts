58dc6dc6460930b1919b62fe6f4a12329e079479
paikpaik <woduf9311@gmail.com>
2023-12-03 21:49:56 +0900

    Add: Custom Pipe 기본 포멧

---
+++ src/auth/pipe/password.pipe.ts
@@ -0,0 +1,13 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+@Injectable()
+export class PasswordPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    return value;
+  }
+}
